5abb02f68c6a244098c62b7030e4a86f31b69554
MarcosSAlonso <marcos.alonso.dsia@gmail.com>
2026-02-20 20:51:03 -0500

    Cargar archivo para testear en server

---
+++ DNRPA SCRAPER.py
@@ -0,0 +1,70 @@
+from playwright.sync_api import sync_playwright
+import time
+
+def runScraper():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=False,
+            args=['--disable-blink-features=AutomationControlled']
+        )
+        
+        page = browser.new_page()
+        page.goto("https://www2.jus.gov.ar/dnrpa-site/#!/estimador")        
+    
+        time.sleep(3)
+
+        resultados = request_info(page)
+
+        if resultados:
+            marca, modelo, anio = resultados
+
+            print(f"Marca: {marca}")
+            print(f"Modelo: {modelo}")
+            print(f"Año: {anio}")
+
+        browser.close()
+
+def request_info(page):
+    try:
+        param = "TRANSFERENCIA"
+        patente = "A211SDM"
+        valor = "1"
+        provincia = "BUENOS AIRES"
+
+        page.select_option("#codigoTramite", param)
+        time.sleep(3)
+
+        #Enviamos patente-dominio
+        page.click("#dominio", force=True)
+        time.sleep(1)
+        page.fill("#dominio", '')
+        time.sleep(2)
+        page.fill("#dominio", patente)
+
+        #Enviamos valor declarado
+        page.click("#cuerpo > div.ng-scope > div > form > div:nth-child(3) > div:nth-child(1) > div > input", force=True)
+        time.sleep(1)
+        page.fill("#cuerpo > div.ng-scope > div > form > div:nth-child(3) > div:nth-child(1) > div > input", '')
+        time.sleep(2)
+        page.fill("#cuerpo > div.ng-scope > div > form > div:nth-child(3) > div:nth-child(1) > div > input", valor)
+
+        #Seleccionamos provincia
+        time.sleep(3)
+        page.select_option("#codigoProvincia", provincia)
+        time.sleep(2)
+
+        #Click continuar
+        page.click("#cuerpo > div.ng-scope > div > form > div:nth-child(5) > button", force=True)
+        time.sleep(3)
+
+        #Guardamos lo obtenido
+        Marca = page.inner_text("#cuerpo > div.ng-scope > div.panel.panel-info.ng-scope > div.panel-body > table > tbody > tr:nth-child(3) > td:nth-child(2)", timeout=5000)
+        Modelo = page.inner_text("#cuerpo > div.ng-scope > div.panel.panel-info.ng-scope > div.panel-body > table > tbody > tr:nth-child(3) > td:nth-child(4)",timeout=5000)
+        Anio = page.inner_text("#cuerpo > div.ng-scope > div.panel.panel-info.ng-scope > div.panel-body > table > tbody > tr:nth-child(4) > td:nth-child(4)", timeout=5000)
+
+        return Marca, Modelo, Anio
+
+    except Exception as e:
+        print("Error obteniendo info de DNRPA: {e}")
+
+runScraper()
