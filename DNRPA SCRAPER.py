370c82ec721f5f116255adc120523a524ab5ee9c
Marcos Alonso <marcos.alonso.dsia@gmail.com>
2026-02-20 21:20:19 -0500

    Update DNRPA SCRAPER.py

--- DNRPA SCRAPER.py
+++ DNRPA SCRAPER.py
@@ -4,7 +4,7 @@ import time
 def runScraper():
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=False,
+            headless=True,
             args=['--disable-blink-features=AutomationControlled']
         )
         
